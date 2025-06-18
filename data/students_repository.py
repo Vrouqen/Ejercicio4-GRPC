0d79b1bb325d45f973f3ae43a972369cbdc08986
Vrouqen <108952975+Vrouqen@users.noreply.github.com>
2025-06-17 19:45:59 -0500

    feat: add data layer

---
+++ data/students_repository.py
@@ -0,0 +1,9 @@
+# This is data layer
+def get_students():
+    # DF simulation
+    return [
+        {"id": 1, "name": "Ana", "grade":10},
+        {"id": 2, "name": "Luis", "grade":7},
+        {"id": 3, "name": "Marta", "grade":8},
+        {"id": 4, "name": "Allan", "grade":2}
+    ]
